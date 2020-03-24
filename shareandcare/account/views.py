5ab8b1f394ffdf88c58bb883d7e9d6d311c4fb02
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-24 16:20:33 +0530

    Add basic home page functionality in the views.py

--- shareandcare/account/views.py
+++ shareandcare/account/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def home_page_view(request):
+    return render(request, 'account/home.html')
