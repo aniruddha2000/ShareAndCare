507cfb81f041ffbdd480fbf6c59ea40bf719db1e
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-25 12:06:35 +0530

    Update the login and signup url

--- shareandcare/account/urls.py
+++ shareandcare/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
 
 
 app_name = 'account'
 urlpatterns = [
     path('', views.home_page_view, name='home'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup'),
 ]
