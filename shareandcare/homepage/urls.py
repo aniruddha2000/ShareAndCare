437c2c8773078e4baab09e925cb3f19b595f287d
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-26 20:13:47 +0530

    Update models urls and views in the homepage app

--- shareandcare/homepage/urls.py
+++ shareandcare/homepage/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 app_name = 'homepage'
 urlpatterns = [
-    # path('home', views.home_page_view, name='homepage'),
+    path('home/<username>', views.home_page_view, name='home'),
 ]
