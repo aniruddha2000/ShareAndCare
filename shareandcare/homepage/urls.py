8f0d02f1c8515c6c42740e3093f169e879a12b56
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-27 13:41:30 +0530

    Update urlpattern

--- shareandcare/homepage/urls.py
+++ shareandcare/homepage/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 app_name = 'homepage'
 urlpatterns = [
-    path('home/<username>', views.home_page_view, name='home'),
+    path('<username>', views.home_page_view, name='home'),
 ]
