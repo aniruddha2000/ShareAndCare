90e71822df5c62919e47667c9ea6b8af0e18eed7
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-29 00:03:57 +0530

    Add view function for my post

--- shareandcare/foodpost/views.py
+++ shareandcare/foodpost/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . import models
 
-# Create your views here.
+
+@login_required(login_url='account:login')
+def my_post_view(request):
+    context = {
+        'my_posts': models.FoodPost.objects.filter(user=request.user),
+        'username': request.user
+    }
+    return render(request, 'foodpost/my_posts.html', context)
