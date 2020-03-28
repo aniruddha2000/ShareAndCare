79a86f2c4b5bda1082d387b9f8aff120d821662c
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-28 23:06:14 +0530

    Update model of foodpost
    Add address field in the foodpost

--- shareandcare/foodpost/models.py
+++ shareandcare/foodpost/models.py
@@ -7,7 +7,7 @@ class FoodPost(models.Model):
     description = models.CharField(max_length=200, default='')
     country = models.CharField(max_length=20, default='')
     city = models.CharField(max_length=30, default='')
-    location_link = models.URLField(default='')
+    address = models.CharField(max_length=300, default='')
 
     def __str__(self):
         return f"{self.user} - {self.title}"
