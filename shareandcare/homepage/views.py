a697d5eb8cb9f3aced6881d5140c7743d5507e0a
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-29 13:11:22 +0530

    Show all posts inside home page exclude the current user

--- shareandcare/homepage/views.py
+++ shareandcare/homepage/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from shareandcare.foodpost import models
 
 
 @login_required(login_url='account:login')
 def home_page_view(request, username):
     user = get_object_or_404(User, username=username)
-    return render(request, 'homepage/homepage.html', {'username': user})
+    args = {
+        'username': user,
+        'all_posts': models.FoodPost.objects.exclude(user=request.user),
+    }
+    return render(request, 'homepage/homepage.html', args)
