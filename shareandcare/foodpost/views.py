3feebd334ad48eb110c9899f5dd4ebc1bfc411e8
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-30 00:13:00 +0530

    Add view for adding new post

--- shareandcare/foodpost/views.py
+++ shareandcare/foodpost/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from . import models
+from . import models, forms
 
 
 @login_required(login_url='account:login')
 def my_post_view(request):
-    context = {
+    args = {
         'my_posts': models.FoodPost.objects.filter(user=request.user),
         'username': request.user
     }
-    return render(request, 'foodpost/my_posts.html', context)
+    return render(request, 'foodpost/my_posts.html', args)
+
+@login_required(login_url='account:login')
+def add_post_view(request):
+    form = forms.FoodPostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('foodpost:mypost')
+    args = {
+        'form': form,
+        'username': request.user
+    }
+    return render(request, 'foodpost/add_post.html', args)
