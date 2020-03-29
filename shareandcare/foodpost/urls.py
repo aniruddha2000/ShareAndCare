c8154390857f5e47d12ee386c50526d603b28800
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-30 00:12:35 +0530

    Add urlpattern for adding new foodpost

--- shareandcare/foodpost/urls.py
+++ shareandcare/foodpost/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 app_name = 'foodpost'
 urlpatterns = [
     path('my-posts', views.my_post_view, name='mypost'),
+    path('add-posts', views.add_post_view, name='addpost'),
 ]
