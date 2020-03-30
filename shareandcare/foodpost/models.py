fcc7dc0b109e03d5327c414831b5462275135b7c
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-03-30 14:34:11 +0530

    Add publishing field for foodpost model

--- shareandcare/foodpost/models.py
+++ shareandcare/foodpost/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class FoodPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=60, default='Excess food')
     description = models.CharField(max_length=200, default='')
+    publishing_date = models.DateTimeField(default=timezone.now, blank=True)
     country = models.CharField(max_length=20, default='')
     city = models.CharField(max_length=30, default='')
     address = models.CharField(max_length=300, default='')
