1e676042f428141288f4b5c381d67d58b3db5c4d
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-04-06 23:50:13 +0530

    Add user profile model and migrate it

--- shareandcare/account/models.py
+++ shareandcare/account/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django_countries.fields import CountryField
+from phone_field import PhoneField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = CountryField()
+    phone = PhoneField()
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
