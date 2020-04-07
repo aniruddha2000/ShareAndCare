dcc75b3fd1e58ad8af4c293eab0fc872e498b9e1
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-04-07 15:09:09 +0530

    Add urlpattern for edit profile information

--- shareandcare/account/urls.py
+++ shareandcare/account/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('signup', views.signup_view, name='signup'),
     path('logout', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('profile/edit', views.edit_profile_view, name='edit_rofile'),
 ]
