1708973d12b9496163dbcf054d05c25b81d8a492
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-04-07 15:09:36 +0530

    Add the edit profile view in the views.py

--- shareandcare/account/views.py
+++ shareandcare/account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 
-from shareandcare.account.forms import *
+from .forms import *
 
 
 def home_view(request):
@@ -43,10 +43,25 @@ def signup_view(request):
 
 @login_required(login_url='account:login')
 def profile_view(request):
+    user_form = EditProfileForm(request.POST or None, instance=request.user)
+    user_profile_form = EditUserProfileForm(request.POST or None, instance=request.user.userprofile)
     args = {
-        "username": request.user
+        'username': request.user,
+        'user_form': user_form,
+        'user_profile_form': user_profile_form,
     }
     return render(request, "account/profile.html", args)
+
+@login_required(login_url='account:login')
+def edit_profile_view(request):
+    user_form = EditProfileForm(request.POST or None, instance=request.user)
+    user_profile_form = EditUserProfileForm(request.POST or None, instance=request.user.userprofile)
+    if user_form.is_valid() and user_profile_form.is_valid():
+        form = user_form.save()
+        custom_form = user_profile_form.save(False)
+        custom_form.user = form
+        custom_form.save()
+        return redirect('account:profile')
 
 @login_required(login_url='account:login')
 def logout_view(request):
